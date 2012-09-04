4fd00b5ad93f3618d89a7c6f419d21e03464c449
newsunshine <newsunshine@4f7b1760-fb07-4c21-afb5-d4d3507be094>
2012-09-04 10:44:54 +0000

    Planification des relevés statistiques

---
+++ deploy/php/cron/remoteCronJob.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Relevé des statistiques
+ */
+define('DEVMODE', false);
+define('URL', 'http://fevermap.org/mushcontagion/php/cron/stat.php');
+
+//Filtre CRON-JOB
+if(!DEVMODE && !isset($_SERVER['CRON_ID']))
+{
+	header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden"); 
+	die("403 Forbidden");
+}
+
+$rawData = file_get_contents(URL);
+$data = simplexml_load_string($rawData);
+
+var_dump($rawData, $data);
+?>
