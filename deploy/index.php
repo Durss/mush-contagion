25ea7606e0c332fffc388fe4bd8c9bb7c5e96d71
François Dursus <durss@free.fr>
2012-09-22 16:47:47 +0000

    Fixing radio form to support quotes and double quotes.

--- deploy/index.php
+++ deploy/index.php
@@ -4,7 +4,13 @@
  * <p>Point névralgique de l'app.</p>
  */
 define('DEVMODE',true);
-define('BETA',false);
+define('BETA', false);
+
+//Anti cache
+header("Cache-Control: no-store, no-cache, must-revalidate"); // HTTP/1.1
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache"); // HTTP/1.0
+
 //Base
 require('php/msg.php');
 require('c/config.php');
